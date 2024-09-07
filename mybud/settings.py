7e2c15ce5e0e94a7035b12eedfadf1f4cca22267
Miracle Ohuka <miralexohuka12@gmail.com>
2024-09-07 09:32:10 +0100

    more deployment edit

--- mybud/settings.py
+++ mybud/settings.py
@@ -136,6 +136,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = "/images/"
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
